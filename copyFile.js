c330fec357b3fdb241110d70bb541be1f3d68ac8
Chris Barrett <chris.barrett@defra.gov.uk>
2021-06-08 12:05:18 +0100

    Switched to CopyFS

---
+++ copyFile.js
@@ -0,0 +1,32 @@
+// Import the filesystem module
+const fs = require('fs');
+
+// Get the current filenames
+// before the function
+getCurrentFilenames();
+console.log("\nFile Contents of example_file:",
+fs.readFileSync("example_file.txt", "utf8"));
+
+// Copying the file to a the same name
+fs.copyFile("example_file.txt", "copyFolder/copied_file.txt", (err) => {
+if (err) {
+	console.log("Error Found:", err);
+}
+else {
+
+	// Get the current filenames
+	// after the function
+	getCurrentFilenames();
+	console.log("\nFile Contents of copied_file:",
+	fs.readFileSync("copyFolder/copied_file.txt", "utf8"));
+}
+});
+
+// Function to get current filenames
+// in directory
+function getCurrentFilenames() {
+console.log("\nCurrent filenames:");
+fs.readdirSync(__dirname).forEach(file => {
+	console.log(file);
+});
+}
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-
-const { src, dest } = require("gulp");
-
-function mytask(callback) {
-   console.log('Hello world!');
-   callback(new Error('Something bad has happened'));
-}
-
-exports.mytask = mytask;
